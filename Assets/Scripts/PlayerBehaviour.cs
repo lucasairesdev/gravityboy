a0ff44c90523d6cfdd96bc7b2a75b56875522d3c
Lucas Aires <116677841+lucasairesdev@users.noreply.github.com>
2023-09-23 17:32:46 -0300

    Player Behaviou finished & gravity script created

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerBehaviour : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class PlayerBehaviour : MonoBehaviour {
+    Rigidbody2D myRigidbody;
+    [SerializeField] float moveSpeed = 4;
+    [SerializeField] float jumpForce = 8;
+    bool isGrounded;
+
+
+
+    private void Start() {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody.velocity = new Vector2(0, 0);
+    }
+
+    public void JumpPlayer() {
+        if (!isGrounded) {
+            return;
+        }
+        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x,jumpForce);
+    }
+    
+    public void MovimentPlayer(bool right) {
+        myRigidbody.velocity = new Vector2(right ? moveSpeed : moveSpeed * -1, myRigidbody.velocity.y);
+    }
+
+    //Inputs para testes de movimentação
+
+    private void Inputs() {
+        if(Input.GetKey(KeyCode.D)) {
+            MovimentPlayer(true);
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            MovimentPlayer(false);
+        }
+        
+
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void InputJump() {
+        if(Input.GetKeyDown(KeyCode.W)) {
+            JumpPlayer();
+        }
+    }
+
+    private void Update() {
+        InputJump();
+    }
+
+    private void FixedUpdate() {
+        Inputs();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) { 
+        isGrounded = false;
     }
 }
